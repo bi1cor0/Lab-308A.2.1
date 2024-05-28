c602225c1cfb054694aafa56fdc83e35787378bd
yeahbudd01 <billcorcoro@comcast.net>
2024-05-27 23:39:21 -0700

    intial commit for the lab

---
+++ class-object-practice.mjs
@@ -0,0 +1,36 @@
+
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+
+    }
+  }
+  
+  const robin = new Character("Robin");
+  robin.inventory = ["sword", "potion", "artifact"];
+  robin.companion = new Character("Leo");
+  robin.companion.type = "Cat";
+  robin.companion.companion = new Character("Frank");
+  robin.companion.companion.type = "Flea";
+  robin.companion.companion.inventory = ["small hat", "sunglasses"];
+  
+class Adventurer extends Character {
+  constructor (name, role) {
+    super(name);
+    // Adventurers have specialized roles.
+    this.role = role;
+    // Every adventurer starts with a bed and 50 gold coins.
+    this.inventory.push("bedroll", "50 gold coins");
+  }
+  // Adventurers have the ability to scout ahead of them.
+  scout () {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
+  }
+}
+
+const altare = new Adventurer("Regis Altare", "Guild Leader")
+console.log(altare)
+console.log(altare.scout())
